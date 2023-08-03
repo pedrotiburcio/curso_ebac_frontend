bd72c6cc8ee59996079590bae32ddad7429b1032
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2023-08-03 11:59:29 -0300

    utilizando filter e arrow function para otimizar o código

--- array_notas.js
+++ array_notas.js
@@ -2,28 +2,8 @@ const alunos = [{nome: 'Pedro', nota: 10}, {nome: 'Antônio', nota: 5}, {nome: '
                 {nome: 'Josy', nota: 5.9}, {nome: 'Wilmar', nota: 6.1},{nome: 'Zenilda', nota: 10 }, 
                 {nome: 'Luciana', nota: 8}, {nome: 'Carlos', nota: 4.5}, {nome: 'Rosa', nota: 6}, {nome: 'Domingos', nota: 9}];
 
-/*function notasMaioresOuIguaisA6(arrayAlunos) {
-    let maioresQue6 = [];
-    arrayAlunos.forEach(function(aluno) {
-        if(aluno.nota >= 6) {
-            maioresQue6.push(aluno);
-        }
-    });
+filtraAprovados = aluno => aluno.nota >= 6;
 
-    return maioresQue6;
-}
+const aprovados = alunos.filter(filtraAprovados);
 
-console.log(notasMaioresOuIguaisA6(alunos)); */
-
-const notasMaioresOuIguaisA6 = (arrayAlunos) => {
-    let maioresQue6 = [];
-    arrayAlunos.forEach((aluno) => {
-        if(aluno.nota >= 6) {
-            maioresQue6.push(aluno);
-        }
-    });
-
-    return maioresQue6;
-}
-
-console.log(notasMaioresOuIguaisA6(alunos));
+console.log(aprovados);
