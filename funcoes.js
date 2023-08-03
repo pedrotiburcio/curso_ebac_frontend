1cd018f8e4a24824528fb4baa649f2228949fafb
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2023-08-02 21:25:50 -0300

    criando funçoes multiplica e sauda

---
+++ funcoes.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplica = (a, b) => a * b;
+const sauda = (nome) => `Olá ${nome}`;
+console.log(multiplica(7, 10));
+console.log(sauda('EBAC'));
---
+++ funcoes.ts
@@ -0,0 +1,5 @@
+const multiplica = (a: number, b: number): number => a * b;
+const sauda = (nome: string): string => `Olá ${nome}`;
+
+console.log(multiplica(7, 10));
+console.log(sauda('EBAC'));
